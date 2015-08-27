b2f4b659cc87eedde05a4f26ddc6884b6f7d5aa6
pranavmayuram <pranav.mayuram@gmail.com>
2015-08-27 16:57:23 -0700

    Completion of v1.0

--- public/js/cpapBank.js
+++ public/js/cpapBank.js
@@ -68,6 +68,12 @@ cpapBank.controller('AppCtrl', function ($scope, $mdSidenav){
 cpapBank.controller('donateController', function ($scope, $http, $window, $state){
 	
 	$scope.machineTypes=["CPAP", "BiPAP", "auto-CPAP", "auto-BiPAP", "ASV", "AVAPS"];
+	$scope.years=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, '50+'];
+
+	$scope.printHiName = function (someObject) {
+		console.log(someObject);
+		console.log('Hi ' + someObject.name + '!');
+	};
 
 });
 
